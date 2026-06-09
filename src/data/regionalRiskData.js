5c147532da803aa7002939cc6071ad2fb18bb9f0
Minjoo Lee <mnjoo.real@gmail.com>
2026-06-09 16:00:19 -0400

    mockdata added in regionalRiskData.js

---
+++ src/data/regionalRiskData.js
@@ -0,0 +1,63 @@
+export const regionalRiskData = [
+  {
+    zipCode: "33101",
+    city: "Miami",
+    state: "FL",
+    floodRisk: 88,
+    wildfireRisk: 18,
+    heatRisk: 84,
+    stormRisk: 92,
+    winterStormRisk: 5
+  },
+  {
+    zipCode: "14623",
+    city: "Rochester",
+    state: "NY",
+    floodRisk: 48,
+    wildfireRisk: 16,
+    heatRisk: 38,
+    stormRisk: 55,
+    winterStormRisk: 82
+  },
+  {
+    zipCode: "90001",
+    city: "Los Angeles",
+    state: "CA",
+    floodRisk: 34,
+    wildfireRisk: 91,
+    heatRisk: 79,
+    stormRisk: 32,
+    winterStormRisk: 4
+  },
+  {
+    zipCode: "77001",
+    city: "Houston",
+    state: "TX",
+    floodRisk: 94,
+    wildfireRisk: 20,
+    heatRisk: 86,
+    stormRisk: 84,
+    winterStormRisk: 6
+  },
+  {
+    zipCode: "80202",
+    city: "Denver",
+    state: "CO",
+    floodRisk: 35,
+    wildfireRisk: 58,
+    heatRisk: 67,
+    stormRisk: 54,
+    winterStormRisk: 68
+  }
+];
+
+export const fallbackRiskData = {
+  zipCode: "00000",
+  city: "Unknown",
+  state: "US",
+  floodRisk: 50,
+  wildfireRisk: 50,
+  heatRisk: 50,
+  stormRisk: 50,
+  winterStormRisk: 50
+};
